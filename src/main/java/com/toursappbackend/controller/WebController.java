fcda166e9d370b05ffc0aa57255d948dbf1e0f55
ICT <brandongrey1324@outlook.com>
2025-05-31 09:32:50 -0700

    06_configure-packaging

---
+++ src/main/java/com/toursappbackend/controller/WebController.java
@@ -0,0 +1,13 @@
+package com.toursappbackend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping({ "/", "/login", "/login/callback", "/groups/**", "/events/**" })
+    public String forwardFrontendRoutes() {
+        return "forward:/index.html";
+    }
+}
